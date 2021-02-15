22ddb1ebb2cae10f93eb85280ecb4c33b5059a60
Daniel Fried <dfried@berkeley.edu>
2021-02-15 10:41:29 -0800

    add poisson rates to entropy calculations

--- entropy.py
+++ entropy.py
@@ -1,7 +1,5 @@
 import torch
-
-N = 5
-D = 20
+from torch import nn
 
 def int_to_bit_array(int_array, num_bits=None, base=2):
     assert int_array.dtype in [torch.int16, torch.int32, torch.int64]
@@ -21,19 +19,30 @@ def int_to_bit_array(int_array, num_bits=None, base=2):
     bits = bits.view((int_array.size()) + (num_bits,))
     return bits
 
-log_X = torch.randn(N,D).log_softmax(-1)
-X = log_X.exp()
+def truncated_poisson_log_pmf(rate, min_support, max_support):
+    rate = torch.tensor(rate)
+    if rate.dim() == 0:
+        rate = rate.unsqueeze(0)
+    if rate.dim() == 1:
+        rate = rate.unsqueeze(1)
+    assert rate.dim() == 2
+    ks = torch.arange(min_support, max_support+1).unsqueeze(0).expand(rate.size(0), -1)
+    logits = ks * torch.log(rate.expand_as(ks)) - rate.expand_as(ks) - (ks + 1.0).lgamma()
+    return logits.log_softmax(-1)
 
-def powerset_sum(X):
-    N, D = X.size()
-    indices = int_to_bit_array(torch.arange(2**D).unsqueeze(0).expand(N, -1).contiguous(), num_bits=D)
-    return torch.einsum("nd,npd->np", X, indices.float())
-
-def powerset_entropy(log_X):
-    log_PX = powerset_sum(log_X)
+def powerset_entropy(log_X, poisson_rate=None):
+    B, D = log_X.size()
+    indices = int_to_bit_array(torch.arange(1, 2**D).unsqueeze(0).expand(B, -1).contiguous(), num_bits=D)
+    log_PX = torch.einsum("nd,npd->np", log_X, indices.float())
+    # B x P
+    num_elts = indices.sum(-1)
+    # B x D
+    if poisson_rate is not None:
+        poisson_log_pmf = truncated_poisson_log_pmf(poisson_rate, 1, D)
+        log_PX = log_PX + poisson_log_pmf.gather(-1, num_elts-1)
     return -(log_PX.exp() * log_PX).sum(-1)
 
-def powerset_entropy_dp(log_X):
+def powerset_entropy_dp(log_X, poisson_rate=None):
     X = log_X.exp()
     B, D = log_X.size()
     alphas = torch.zeros(B, D, D)
@@ -47,6 +56,40 @@ def powerset_entropy_dp(log_X):
             beta_rec = betas[:,n-1,k-1]
             betas[:,n,k] = betas[:,n,k-1] + K * beta_rec
             alphas[:,n,k] = alphas[:,n,k-1] + K * alphas[:,n-1,k-1] + beta_rec * K * log_K
-    return -alphas[:,:,-1].sum(-1)
+    per_size_probs = betas[:,:,-1]
+    per_size_entropies = -alphas[:,:,-1]
+    if poisson_rate is not None:
+        poisson_log_pmf = truncated_poisson_log_pmf(poisson_rate, 1, D)
+        poisson_per_size_entropies = -(poisson_log_pmf * poisson_log_pmf.exp())
+        per_size_entropies = per_size_entropies * poisson_log_pmf.exp() + per_size_probs * poisson_per_size_entropies
+    return per_size_entropies.sum(-1)
 
-#entropy = torch.einsum(
+class SetDistribution:
+    def __init__(self, logits, untruncated_poisson_mean):
+        self.logits = logits
+        self.untruncated_poisson_mean = untruncated_poisson_mean
+
+    def entropy(self):
+        return powerset_entropy_dp(self.logits.log_softmax(-1))
+
+    def sample(self, num_samples):
+        pass
+
+
+N = 5
+D = 15
+
+log_X = torch.randn(N,D).log_softmax(-1)
+X = log_X.exp()
+
+H_no_rates = powerset_entropy(log_X)
+H_dp_no_rates = powerset_entropy_dp(log_X)
+
+assert torch.allclose(H_no_rates, H_dp_no_rates, atol=1e-3)
+
+rates = torch.full((N, 1), 1.0)
+
+H = powerset_entropy(log_X, rates)
+H_dp = powerset_entropy_dp(log_X, rates)
+
+assert torch.allclose(H, H_dp, atol=1e-3)
